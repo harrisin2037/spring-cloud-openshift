753596765270e8d6d75194631e48733a91ef7daa
harrisin <harrisin2037@gmail.com>
2021-10-03 17:48:01 +0800

    sh file for clean the secret

---
+++ clean.sh
@@ -0,0 +1,6 @@
+kubectl delete pipeline spring-cloud-transaction
+kubectl delete imagestream spring-cloud-transaction
+kubectl delete deploymentconfig spring-cloud-transaction
+kubectl delete buildconfig spring-cloud-transaction
+kubectl delete service spring-cloud-transaction
+kubectl delete route spring-cloud-transaction
