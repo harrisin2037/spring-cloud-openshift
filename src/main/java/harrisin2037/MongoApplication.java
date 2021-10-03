99b5302109687664cc166e9d8bac54de22ae4b24
harrisin <harrisin2037@gmail.com>
2021-10-03 19:43:23 +0800

    mongo auto config

--- src/main/java/harrisin2037/MongoApplication.java
+++ src/main/java/harrisin2037/MongoApplication.java
@@ -1,11 +1,14 @@
 package harrisin2037;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class MongoApplication extends SpringBootServletInitializer {
   public static void main(String[] args) {
     SpringApplication.run(MongoApplication.class, args);
